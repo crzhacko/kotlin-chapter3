58e2398807999d1afbb366311c88192970fd3f8e
crzhacko <crzhacko@gmail.com>
2019-11-28 23:20:37 +0900

    Before deserializing

--- src/main/kotlin/com/microservices/chapter3/Chapter3Application.kt
+++ src/main/kotlin/com/microservices/chapter3/Chapter3Application.kt
@@ -6,17 +6,18 @@ import org.springframework.context.annotation.Bean
 import java.util.concurrent.ConcurrentHashMap
 
 @SpringBootApplication
-open class Chapter3Application {
-	companion object {
-		val initialCustomers = arrayOf(Customer(1, "Kotlin"),
-				Customer(2, "Spring"),
-				Customer(3, "Microservice"))
-	}
-
-	@Bean
-	open fun customers() = ConcurrentHashMap<Int, Customer>(initialCustomers.associateBy(Customer::id))
-
-}
+open class Chapter3Application
+//{
+//	companion object {
+//		val initialCustomers = arrayOf(Customer(1, "Kotlin"),
+//				Customer(2, "Spring"),
+//				Customer(3, "Microservice"))
+//	}
+//
+//	@Bean
+//	open fun customers() = ConcurrentHashMap<Int, Customer>(initialCustomers.associateBy(Customer::id))
+//
+//}
 
 fun main(args: Array<String>) {
 	runApplication<Chapter3Application>(*args)
--- src/main/kotlin/com/microservices/chapter3/CustomerController.kt
+++ src/main/kotlin/com/microservices/chapter3/CustomerController.kt
@@ -1,6 +1,8 @@
 package com.microservices.chapter3
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import java.util.concurrent.ConcurrentHashMap
 
@@ -8,30 +10,43 @@ import java.util.concurrent.ConcurrentHashMap
 class CustomerController {
 
     @Autowired
-    lateinit var customers : ConcurrentHashMap<Int, Customer>
+    private lateinit var customerService : CustomerService
 
     @GetMapping(value = arrayOf("/customers"))
-    fun  getCustomers(@RequestParam(required = false, defaultValue = "") nameFilter: String) =
-            customers.filter {
-                it.value.name.contains(nameFilter, true)
-            }.map(Map.Entry<Int, Customer>::value).toList()
+    fun getCustomers(@RequestParam(required = false, defaultValue = "") nameFilter: String) =
+            customerService.searchCustomers(nameFilter)
 
     @GetMapping(value = arrayOf("/customer/{id}"))
-    fun getCustomer(@PathVariable id : Int) = customers[id]
+    fun getCustomer(@PathVariable id : Int): ResponseEntity<Customer?> {
+        val customer = customerService.getCustomer(id)
+        val status = if (customer == null) HttpStatus.NOT_FOUND else HttpStatus.OK
+        return ResponseEntity(customer, status)
+    }
 
     @PostMapping(value = arrayOf("/customer/"))
-    fun createCustomer(@RequestBody customer: Customer) {
-        customers[customer.id] = customer
+    fun createCustomer(@RequestBody customer: Customer): ResponseEntity<Unit?> {
+        customerService.createCustomer(customer)
+        return ResponseEntity(null, HttpStatus.CREATED)
     }
 
     @DeleteMapping(value = arrayOf("/customer/{id}"))
-    fun deleteCustomer(@PathVariable id: Int) = customers.remove(id)
+    fun deleteCustomer(@PathVariable id: Int): ResponseEntity<Unit?> {
+        var status = HttpStatus.NOT_FOUND
+        if (customerService.getCustomer(id) != null) {
+            customerService.deleteCustomer(id)
+            status = HttpStatus.OK
+        }
+        return ResponseEntity(null, status)
+    }
 
     @PutMapping(value = arrayOf("/customer/{id}"))
-    fun updateCustomer(@PathVariable id: Int, @RequestBody customer: Customer)
-    {
-        customers.remove(id)
-        customers[customer.id] = customer
+    fun updateCustomer(@PathVariable id: Int, @RequestBody customer: Customer): ResponseEntity<Unit?> {
+        var status = HttpStatus.NOT_FOUND
+        if (customerService.getCustomer(id) != null) {
+            customerService.updateCustomer(id, customer)
+            status = HttpStatus.ACCEPTED
+        }
+        return ResponseEntity(null, status)
     }
 }
 
